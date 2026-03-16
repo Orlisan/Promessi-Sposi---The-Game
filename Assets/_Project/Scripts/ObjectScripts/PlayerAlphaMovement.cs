75ca25c9925ba3434ad68bc95de61c0256d63bf3
Orlisan <sangiovanniorlando12@gmail.com>
2026-03-16 12:01:59 +0100

    rwyrersfgn
    Sitesmata telecamera

--- Assets/_Project/Scripts/ObjectScripts/PlayerAlphaMovement.cs
+++ Assets/_Project/Scripts/ObjectScripts/PlayerAlphaMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerAlphaMovement : MonoBehaviour
 {
+    public float diQuantoLaCameraVieneSpostataAllaRotazioneDellaCameraEffettuataColMouseInTerzaPersona = 1f;
     public float quantitàDiVelocitàPersaQuandoSiRilasciaIlTastoPerSaltareMaTipoCheLaVelocitàVieneMoltiplicataPerUnNumeroDecimale = 0.5f; //AVANTI, GIUDICAMI >:(
     public Rigidbody rb;
     public float jumpPower = 16;
@@ -10,8 +11,11 @@ public class PlayerAlphaMovement : MonoBehaviour
     public float velocita = 5f;
     public float sensibilita = 2f;
     public Vector3 offset = new Vector3(0, 1, 1);
+    public Vector3 thirdyPersonOffset = new Vector3(0, 0, -10);
     public bool thirdyPerson = false;
     public float rotazioneX = 0f;
+    float mouseX = 0f;
+    float mouseY = 0f;
     public float rotazioneY = 0f;
     private bool isGrounded = false;
     private bool isPressed = false;
@@ -28,8 +32,8 @@ void Start()
     
  void Update()
 {
-    float mouseX = Input.GetAxis("Mouse X") * sensibilita;
-    float mouseY = Input.GetAxis("Mouse Y") * sensibilita;
+    mouseX = Input.GetAxis("Mouse X") * sensibilita;
+    mouseY = Input.GetAxis("Mouse Y") * sensibilita;
 
     rotazioneY += mouseX;
     rotazioneX -= mouseY;
@@ -37,9 +41,20 @@ void Start()
 
     transform.rotation = Quaternion.Euler(0, rotazioneY, 0);
     camera.transform.localRotation = Quaternion.Euler(rotazioneX, 0, 0);
+    if(thirdyPerson) {
+        
+        float rotazioneXRadianti = rotazioneX * Mathf.Deg2Rad;
+        float tangenteNonProporzionata = Mathf.Tan(rotazioneXRadianti);
+        float tangenteProporzionata = offset.z * tangenteNonProporzionata;
+        Vector3 offsetConCorrezioneAltezza = new Vector3(offset.x, offset.y - tangenteProporzionata, offset.z);
+        camera.transform.position = transform.position + transform.TransformDirection(offsetConCorrezioneAltezza);
+    }
+    
+        
 }
 void FixedUpdate()
 {
+    
     if(cool < 300 && startCool) {
         cool += 50;
     } else
@@ -87,7 +102,7 @@ if (Input.GetKey(KeyCode.F5))
         {if(cool == 0) {
             if(!thirdyPerson)
             {
-                offset = new Vector3(0, 5, -10);
+                offset = thirdyPersonOffset;
                 camera.transform.Rotate(0, -60, 0);
             }else
             {
@@ -97,6 +112,10 @@ if (Input.GetKey(KeyCode.F5))
             
             thirdyPerson = !thirdyPerson;
             startCool = true;
+           
+            if(thirdyPerson) {
+                offset = new Vector3(offset.x, offset.y + -mouseY,offset.z + -mouseX);
+            }   
         }
         }
 Vector3 pos = new Vector3(x, y, z);
