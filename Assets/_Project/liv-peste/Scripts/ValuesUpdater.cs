08f122aa38a73fb193ecd74aecad50a2af1fad33
Orlisan <sangiovanniorlando12@gmail.com>
2026-03-30 21:07:10 +0200

    Sync UI sliders from Dati in ValuesUpdater
    Add real-time slider updates to ValuesUpdater: include UnityEngine.UI, expose two public Slider fields (sliderPeste, sliderProvvidenza) and set their values each frame from Dati.livelloPeste and Dati.livelloProvvidenza. Update Alpha_Scene.unity to add the ValuesUpdater MonoBehaviour instance and wire the slider references.

--- Assets/_Project/liv-peste/Scripts/ValuesUpdater.cs
+++ Assets/_Project/liv-peste/Scripts/ValuesUpdater.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ValuesUpdater : MonoBehaviour
 {
+    public Slider sliderPeste;
+    public Slider sliderProvvidenza;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,8 @@ public class ValuesUpdater : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        sliderPeste.value = Dati.livelloPeste;
+        sliderProvvidenza.value = Dati.livelloProvvidenza;
+
     }
 }
